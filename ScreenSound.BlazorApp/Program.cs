254806900d0a5bd2e21744991cbe3f575c1f8e51
bessax <andrebessax@gmail.com>
2023-11-09 12:00:42 -0300

    #Ajustes: - Alterar o Index para exibir os últimos artistas cadastrados; - Criar uma pasta Services, para colocar o ArtistaAPI, classe que irá consumir nossa API; - Resolver a referencias da entidade artista adicionando uma referência de projeto para Shared.Modelos; - Definir a injeção de dependência do HttpClient e adicionar a biblioteca Microsoft.Extensions.Http no projeto; - Editar o index e definir o inject ArtistaAPI; - No _Imports definir `@using ScreenSound.BlazorApp.Servicos`

--- ScreenSound.BlazorApp/Program.cs
+++ ScreenSound.BlazorApp/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScreenSound.BlazorApp;
+using ScreenSound.BlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<ArtistaAPI>(client => {
+    client.BaseAddress = new Uri("https://localhost:7016");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 await builder.Build().RunAsync();
